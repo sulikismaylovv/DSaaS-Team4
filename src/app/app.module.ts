98c7d614855b7541c1dc258e6e057d4691240280
Suleyman Ismayilov <suleyman.ismayilov@student.kuleuven.be>
2023-10-12 21:00:57 +0200

    supabase added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core'
+import { BrowserModule } from '@angular/platform-browser'
 
-import { AppComponent } from './app.component';
+import { AppComponent } from './app.component'
+import { AuthComponent } from './auth/auth.component'
+import { AccountComponent } from './account/account.component'
+import { ReactiveFormsModule } from '@angular/forms'
+import { AvatarComponent } from './avatar/avatar.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, AuthComponent, AccountComponent, AvatarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
