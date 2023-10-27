b6d7bb856f364dda8b1da021751b3b8e0ed6c516
Suleyman Ismayilov <ismayilov.suleyman0304@gmail.com>
2023-10-27 15:38:01 +0200

    CI/CD check on main

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
   supabaseUrl: 'https://kvjhoifmabxitkelfeod.supabase.co',
-  appTitle: 'My Title (production)',
+  appTitle: 'Remontada (production)',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2amhvaWZtYWJ4aXRrZWxmZW9kIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcxMzQ3NDMsImV4cCI6MjAxMjcxMDc0M30.vttAJafQbMLkJFQU0r49cRjKa9fHcmtIQYkcNyAo5A4',
 }
