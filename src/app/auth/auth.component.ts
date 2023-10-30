1e76788690578e98d4660972dac1a6a2e8f5bf84
Suleyman Ismayilov <ismayilov.suleyman0304@gmail.com>
2023-10-30 12:39:21 +0100

    New Values added to User

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core'
-import { FormBuilder } from '@angular/forms'
-import { SupabaseService } from '../supabase.service'
+import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { SupabaseService } from '../supabase.service';
 
 @Component({
   selector: 'app-auth',
@@ -8,10 +8,16 @@ import { SupabaseService } from '../supabase.service'
   styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent {
-  loading = false
+  loading = false;
 
+  // Add password field to the form
   signInForm = this.formBuilder.group({
     email: '',
+    password: '',
+  });
+
+  signInFormEmail = this.formBuilder.group({
+    email2: '',
   })
 
   constructor(
@@ -21,9 +27,30 @@ export class AuthComponent {
 
   async onSubmit(): Promise<void> {
     try {
+      this.loading = true;
+      const email = this.signInForm.value.email as string;
+      const password = this.signInForm.value.password as string;
+
+      // Register the user
+      const { error } = await this.supabase.register(email, password);
+      if (error) throw error;
+      alert('Check your email for the confirmation link!');
+
+    } catch (error) {
+      if (error instanceof Error) {
+        alert(error.message);
+      }
+    } finally {
+      this.signInForm.reset();
+      this.loading = false;
+    }
+  }
+
+  async onSubmitEmail(): Promise<void> {
+    try {
       this.loading = true
-      const email = this.signInForm.value.email as string
-      const { error } = await this.supabase.signIn(email)
+      const email2 = this.signInForm.value.email as string
+      const { error } = await this.supabase.signIn(email2)
       if (error) throw error
       alert('Check your email for the login link!')
     } catch (error) {
--- src/app/supabase.service.ts
+++ src/app/supabase.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@angular/core'
+import { Injectable } from '@angular/core';
 import {
   AuthChangeEvent,
   AuthSession,
@@ -6,68 +6,77 @@ import {
   Session,
   SupabaseClient,
   User,
-} from '@supabase/supabase-js'
-import { environment } from 'src/environments/environment'
+} from '@supabase/supabase-js';
+import { environment } from 'src/environments/environment';
 
 export interface Profile {
-  id?: string
-  username: string
-  website: string
-  avatar_url: string
+  id?: string;
+  username: string;
+  avatar_url: string;
+  birthdate?: Date;
+  password?: string; // Note: It's not recommended to handle raw passwords in client-side code.
+  first_name: string;
+  last_name: string;
+  // Add any other fields as needed
 }
 
 @Injectable({
   providedIn: 'root',
 })
 export class SupabaseService {
-  private supabase: SupabaseClient
-  _session: AuthSession | null = null
+  private supabase: SupabaseClient;
+  _session: AuthSession | null = null;
 
   constructor() {
-    this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey)
+    this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
   }
 
   get session() {
     this.supabase.auth.getSession().then(({ data }) => {
-      this._session = data.session
-    })
-    return this._session
+      this._session = data.session;
+    });
+    return this._session;
   }
 
   profile(user: User) {
     return this.supabase
-      .from('profiles')
-      .select(`username, website, avatar_url`)
+      .from('users') // Changed from 'profiles' to 'users'
+      .select(`username, avatar_url, birthdate, first_name, last_name`) // Updated fields
       .eq('id', user.id)
-      .single()
+      .single();
   }
 
   authChanges(callback: (event: AuthChangeEvent, session: Session | null) => void) {
-    return this.supabase.auth.onAuthStateChange(callback)
+    return this.supabase.auth.onAuthStateChange(callback);
   }
 
+  async register(email: string, password: string) {
+    return this.supabase.auth.signUp({ email, password });
+  }
+
+
   signIn(email: string) {
-    return this.supabase.auth.signInWithOtp({ email })
+    return this.supabase.auth.signInWithOtp({ email });
   }
 
   signOut() {
-    return this.supabase.auth.signOut()
+    return this.supabase.auth.signOut();
   }
 
   updateProfile(profile: Profile) {
     const update = {
       ...profile,
       updated_at: new Date(),
-    }
+    };
 
-    return this.supabase.from('profiles').upsert(update)
+    return this.supabase.from('users').upsert(update); // Changed from 'profiles' to 'users'
   }
 
   downLoadImage(path: string) {
-    return this.supabase.storage.from('avatars').download(path)
+    return this.supabase.storage.from('avatars').download(path);
   }
 
   uploadAvatar(filePath: string, file: File) {
-    return this.supabase.storage.from('avatars').upload(filePath, file)
+    return this.supabase.storage.from('avatars').upload(filePath, file);
   }
 }
