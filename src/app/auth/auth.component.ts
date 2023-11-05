c31db71c2b60631444444531926b709764f11d76
Suleyman Ismayilov <suleyman.ismayilov@student.kuleuven.be>
2023-11-05 18:20:02 +0100

    Validators added to the register and few changes to regitration method

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import {FormBuilder, FormGroup} from '@angular/forms';
 import { SupabaseService } from '../supabase.service';
+import {ReactiveFormsModule , Validators } from "@angular/forms";
 
 @Component({
   selector: 'app-auth',
@@ -8,33 +9,71 @@ import { SupabaseService } from '../supabase.service';
   styleUrls: ['./auth.component.css'],
 })
 export class AuthComponent {
+  registerForm!: FormGroup;
   loading = false;
 
-  // Add password field to the form
+  // Separate form groups for sign-in and registration
   signInForm = this.formBuilder.group({
     email: '',
     password: '',
   });
 
-  constructor(
-    private readonly supabase: SupabaseService,
-    private readonly formBuilder: FormBuilder
-  ) {}
+  matchingPasswords(group: FormGroup): { [key: string]: any } | null {
+    const password = group.controls['password'].value;
+    const confirmPassword = group.controls['confirmPassword'].value;
+    return password === confirmPassword ? null : { 'mismatchedPasswords': true };
+  }
 
-  async onSubmit(): Promise<void> {
+  constructor(
+
+    private readonly supabase: SupabaseService,
+
+    private formBuilder: FormBuilder) {
+      this.registerForm = this.formBuilder.group({
+        email: formBuilder.control('', [Validators.required, Validators.email, Validators.minLength(5)]),
+        password: ['', [
+          Validators.required,
+          Validators.minLength(8), // Minimum length for the password
+          Validators.pattern('^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{8,}$') // Passwords must contain at least 1 uppercase letter, 1 lowercase letter, and 1 number
+        ]],
+        confirmPassword: ['', Validators.required]
+      }, { validator: this.matchingPasswords });
+
+  }
+
+
+  // Registration method
+  async onRegister(): Promise<void> {
+    try {
+      this.loading = true;
+      const email = this.registerForm.value.email as string;
+      const password = this.registerForm.value.password as string;
+
+      const response = await this.supabase.register(email, password);
+      if (response.error) throw response.error;
+
+      alert('You are successfully registered!');
+    } catch (error) {
+      if (error instanceof Error) {
+        alert(error.message);
+      }
+    } finally {
+      this.registerForm.reset();
+      this.loading = false;
+    }
+  }
+
+  // Sign-in method
+  async onSignIn(): Promise<void> {
     try {
       this.loading = true;
       const email = this.signInForm.value.email as string;
       const password = this.signInForm.value.password as string;
 
-      // Register the user
-      let response = await this.supabase.register(email, password);
+      const response = await this.supabase.signIn(email, password); // Adjust the signIn method to accept password
       if (response.error) throw response.error;
 
-      response = await this.supabase.signIn(email);
-      if (response.error) throw response.error;
-
-      alert('Check your email for the confirmation link!');
+      // Handle successful sign-in
     } catch (error) {
       if (error instanceof Error) {
         alert(error.message);
--- src/app/supabase.service.ts
+++ src/app/supabase.service.ts
@@ -1,82 +1,83 @@
-import { Injectable } from '@angular/core';
-import {
-  AuthChangeEvent,
-  AuthSession,
-  createClient,
-  Session,
-  SupabaseClient,
-  User,
-} from '@supabase/supabase-js';
-import { environment } from 'src/environments/environment';
+  import { Injectable } from '@angular/core';
+  import {
+    AuthChangeEvent,
+    AuthSession,
+    createClient,
+    Session,
+    SupabaseClient,
+    User,
+  } from '@supabase/supabase-js';
+  import { environment } from 'src/environments/environment';
 
-export interface Profile {
-  id?: string;
-  username: string;
-  avatar_url: string;
-  birthdate?: Date;
-  password?: string; // Note: It's not recommended to handle raw passwords in client-side code.
-  first_name: string;
-  last_name: string;
-  // Add any other fields as needed
-}
-
-@Injectable({
-  providedIn: 'root',
-})
-export class SupabaseService {
-  private supabase: SupabaseClient;
-  _session: AuthSession | null = null;
-
-  constructor() {
-    this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
+  export interface Profile {
+    id?: string;
+    username: string;
+    avatar_url: string;
+    birthdate?: Date;
+    password?: string; // Note: It's not recommended to handle raw passwords in client-side code.
+    first_name: string;
+    last_name: string;
+    // Add any other fields as needed
   }
 
-  get session() {
-    this.supabase.auth.getSession().then(({ data }) => {
-      this._session = data.session;
-    });
-    return this._session;
+  @Injectable({
+    providedIn: 'root',
+  })
+  export class SupabaseService {
+    private supabase: SupabaseClient;
+    _session: AuthSession | null = null;
+
+    constructor() {
+      this.supabase = createClient(environment.supabaseUrl, environment.supabaseKey);
+    }
+
+    get session() {
+      this.supabase.auth.getSession().then(({ data }) => {
+        this._session = data.session;
+      });
+      return this._session;
+    }
+
+    profile(user: User) {
+      return this.supabase
+        .from('users') // Changed from 'profiles' to 'users'
+        .select(`username, avatar_url, birthdate, first_name, last_name`) // Updated fields
+        .eq('id', user.id)
+        .single();
+    }
+
+    authChanges(callback: (event: AuthChangeEvent, session: Session | null) => void) {
+      return this.supabase.auth.onAuthStateChange(callback);
+    }
+
+    async register(email: string, password: string) {
+      return this.supabase.auth.signUp({ email, password });
+    }
+
+
+    async signIn(email: string, password: string) {
+      return this.supabase.auth.signInWithPassword({ email, password });
+    }
+
+
+    signOut() {
+      return this.supabase.auth.signOut();
+    }
+
+    updateProfile(profile: Profile) {
+      const update = {
+        ...profile,
+        updated_at: new Date(),
+      };
+
+      return this.supabase.from('users').upsert(update); // Changed from 'profiles' to 'users'
+    }
+
+    downLoadImage(path: string) {
+      return this.supabase.storage.from('avatars').download(path);
+    }
+
+    uploadAvatar(filePath: string, file: File) {
+      return this.supabase.storage.from('avatars').upload(filePath, file);
+    }
   }
-
-  profile(user: User) {
-    return this.supabase
-      .from('users') // Changed from 'profiles' to 'users'
-      .select(`username, avatar_url, birthdate, first_name, last_name`) // Updated fields
-      .eq('id', user.id)
-      .single();
-  }
-
-  authChanges(callback: (event: AuthChangeEvent, session: Session | null) => void) {
-    return this.supabase.auth.onAuthStateChange(callback);
-  }
-
-  async register(email: string, password: string) {
-    return this.supabase.auth.signUp({ email, password });
-  }
-
-
-  signIn(email: string) {
-    return this.supabase.auth.signInWithOtp({ email });
-  }
-
-  signOut() {
-    return this.supabase.auth.signOut();
-  }
-
-  updateProfile(profile: Profile) {
-    const update = {
-      ...profile,
-      updated_at: new Date(),
-    };
-
-    return this.supabase.from('users').upsert(update); // Changed from 'profiles' to 'users'
-  }
-
-  downLoadImage(path: string) {
-    return this.supabase.storage.from('avatars').download(path);
-  }
-
-  uploadAvatar(filePath: string, file: File) {
-    return this.supabase.storage.from('avatars').upload(filePath, file);
-  }
-}
