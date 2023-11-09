1edf463b94bb30feea4ee9dae166e367975d9226
LAPTOP-CE9FDRAL\Eigenaar <kasper.dejans@student.kuleuven.be>
2023-11-09 19:09:00 +0100

    Posts and Matches on the same page
    I added two components: posts and matches. These are accessed through routing and now we can switch between these using buttons.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {RouterModule, Routes } from '@angular/router';
+import {PostsComponent} from "./posts/posts.component";
+import {MatchesComponent} from "./matches/matches.component";
 
-
-
+const routes: Routes = [
+  { path: 'posts', component: PostsComponent },
+  { path: 'games', component: MatchesComponent },
+  { path: '', redirectTo: '/posts', pathMatch: 'full' }, // Redirect to posts by default
+  { path: '**', redirectTo: '/posts', pathMatch: 'full' }, // Redirect to posts for any other route
+];
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    CommonModule,
+    RouterModule.forRoot(routes)
+  ],
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,14 +6,17 @@ import { AuthComponent } from './auth/auth.component'
 import { AccountComponent } from './account/account.component'
 import { ReactiveFormsModule } from '@angular/forms'
 import { AvatarComponent } from './avatar/avatar.component';
-import { AppRoutingModule } from './app-routing.module'
+import { AppRoutingModule } from './app-routing.module';
+import { PostsComponent } from './posts/posts.component'
+import {RouterLink, RouterLinkActive} from "@angular/router";
+import { MatchesComponent } from './matches/matches.component';
 
 @NgModule({
   declarations: [
-    AppComponent, AuthComponent, AccountComponent, AvatarComponent
+    AppComponent, AuthComponent, AccountComponent, AvatarComponent, PostsComponent, MatchesComponent
   ],
   imports: [
-    BrowserModule, ReactiveFormsModule, AppRoutingModule
+    BrowserModule, ReactiveFormsModule, AppRoutingModule, RouterLink, RouterLinkActive
   ],
   providers: [],
   bootstrap: [AppComponent],
