12d3c47ef2e53bd4770126568a05279f4fd50477
boogle-noogle <abdelnaser.abdoa@gmail.com>
2023-11-15 16:57:32 +0100

    Working matches with next and previous button

--- src/app/matches/matches.component.ts
+++ src/app/matches/matches.component.ts
@@ -9,21 +9,46 @@ import { initFlowbite } from 'flowbite';
   templateUrl: './matches.component.html',
   styleUrls: ['./matches.component.css']
 })
-export class MatchesComponent implements OnInit{
+export class MatchesComponent implements OnInit {
 
-  constructor(private apiService: ApiService) { }
   fixtures: Fixture[] = [];
+  currentDate: Date;
+  stringDate: string;
+
+  constructor(private apiService: ApiService) {
+    // this.currentDate = new Date(); //use this for today's date
+    this.currentDate = new Date('2023-11-12'); //use this for specific date
+    this.stringDate = this.currentDate.toISOString().split('T')[0];
+  }
 
   ngOnInit() {
-    this.parseFixtures(144, '2023-10-22');
+    this.parseFixtures(144);
 
   }
 
-  parseFixtures(leagueID: number, date: string) {
-    this.apiService.fetchFixtures(leagueID, date).subscribe(
+  parseFixtures(leagueID: number) {
+    let dateString = this.getDateAsString(this.currentDate);
+    console.log(dateString);
+    this.apiService.fetchFixtures(leagueID, dateString).subscribe(
       (data: Fixture[]) => {
         this.fixtures = data;
         console.log(this.fixtures)
       })
+  }
+
+  goToNextDay() {
+    this.currentDate.setDate(this.currentDate.getDate() + 1);
+    this.parseFixtures(144);
+  }
+
+  goToPreviousDay() {
+    this.currentDate.setDate(this.currentDate.getDate() - 1);
+    this.parseFixtures(144);
+  }
+
+  getDateAsString(date: Date): string {
+    const offset = date.getTimezoneOffset();
+    const adjustedDate = new Date(date.getTime() - offset * 60 * 1000);
+    return adjustedDate.toISOString().split('T')[0];
   }
 }
