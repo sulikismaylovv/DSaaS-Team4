60fa52a942c9d27e727ebd2fb8fbd01d930d1fab
Suleyman Ismayilov <suleyman.ismayilov@student.kuleuven.be>
2023-11-17 14:22:15 +0100

    product url changed

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   supabaseUrl: 'https://kvjhoifmabxitkelfeod.supabase.co',
   supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2amhvaWZtYWJ4aXRrZWxmZW9kIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcxMzQ3NDMsImV4cCI6MjAxMjcxMDc0M30.vttAJafQbMLkJFQU0r49cRjKa9fHcmtIQYkcNyAo5A4',
-  baseUrl: 'https://d-saas-team4.vercel.app/',
+  baseUrl: 'https://remontada.live/',
     appTitle: ''
 }
