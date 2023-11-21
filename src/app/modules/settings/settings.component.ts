3ced2fbad980f9508a23f93e5a44b617d67caf24
IvanDeyanov <146430478+IvanDeyanov@users.noreply.github.com>
2023-11-21 22:14:53 +0100

    new settings design

--- src/app/modules/settings/settings.component.ts
+++ src/app/modules/settings/settings.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
   selector: 'app-settings',
@@ -8,6 +8,19 @@ import { Component } from '@angular/core';
 export class SettingsComponent {
   showContent: boolean = false;
   clickedImage: string | null = null;
+  @Input() team: any;
+  @Input() selected: boolean = false;
+  @Input() favorite: boolean = false; // Add this line to represent a favorite team
+  @Output() selectTeam = new EventEmitter<any>();
+  hover: boolean = false;
+
+  toggleSelection() {
+    // If you want to prevent changing the selection of the favorite team,
+    // you can add a condition here:
+    if (!this.favorite) {
+      this.selectTeam.emit(this.team);
+    }
+  }
   toggleContent(team: string) {
     if (this.clickedImage === team) {
       // If the same team is clicked again, reset everything
@@ -17,6 +30,8 @@ export class SettingsComponent {
       // Otherwise, show content and set the clicked team
       this.showContent = true;
       this.clickedImage = team;
+
+
     }}
 
 }
