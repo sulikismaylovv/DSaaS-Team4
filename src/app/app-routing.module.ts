cbd7a04f233cc1dbeebb79e9d32260ac2f89ac4a
chrysovalantis.psilos <chrysovalantis.psilos@student.kuleuven.be>
2023-11-22 14:15:01 +0100

    footer is now hidden for all pages except login and register

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,7 +20,7 @@ const routes: Routes = [
     {path: 'dashboard', component: DashboardComponent, canActivate: [ProfileGuard]},
     {path: 'register', component: AuthComponent, pathMatch: 'full'},
     {path: 'complete-profile', component: MultistepformComponent, pathMatch: 'full'},
-    {path: '', redirectTo: '/shop', pathMatch: 'full',},
+    {path: '', redirectTo: '/login', pathMatch: 'full',},
     {path: 'home', component: HomeComponent, pathMatch: 'full'},
     {path: 'verify-email', component: VerifyEmailComponent, pathMatch: 'full'},
     {path: 'game', component: GameComponent, pathMatch: 'full'},
--- src/app/shared/app.component.ts
+++ src/app/shared/app.component.ts
@@ -3,8 +3,9 @@ import {AuthService} from '../core/services/auth.service';
 import {initFlowbite} from 'flowbite';
 import {environment} from '../../environments/environment';
 import {ThemeService} from '../core/services/theme.service';
-import {Router} from '@angular/router';
+import {Router, NavigationEnd} from '@angular/router';
 import {GameComponent} from "../modules/game_module/game.component";
+import {FooterService} from "../core/services/footer.service";
 
 @Component({
     selector: 'app-root',
@@ -16,17 +17,21 @@ export class AppComponent implements OnInit {
     session: any; // Adjust the type based on your session object
     protected readonly GameComponent = GameComponent;
 
-    constructor(
-        private router: Router,
-        protected readonly authService: AuthService,
-        public themeService: ThemeService
-    ) {
-    }
+    constructor(private router: Router, private footerService: FooterService, protected readonly authService: AuthService, public themeService: ThemeService) {}
 
     ngOnInit() {
-        // Subscribe to the auth state changes
-        this.authService.authChanges((_, session) => (this.session = session));
-        initFlowbite();
+      // Subscribe to the auth state changes
+      this.authService.authChanges((_, session) => (this.session = session));
+      initFlowbite();
+      this.router.events.subscribe(event => {
+        if (event instanceof NavigationEnd) {
+          if (event.urlAfterRedirects === '/login' || event.urlAfterRedirects === '/register') {
+            this.footerService.setShowFooter(true);
+          } else {
+            this.footerService.setShowFooter(false);
+          }
+        }
+      });
     }
 
     toggleTheme() {
