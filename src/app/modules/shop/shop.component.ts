3b55032929bb18b34f2fd9635816d686c0a74145
chrysovalantis.psilos <chrysovalantis.psilos@student.kuleuven.be>
2023-11-22 13:42:30 +0100

    added ability to hide and show more badges

--- src/app/modules/shop/shop.component.ts
+++ src/app/modules/shop/shop.component.ts
@@ -6,5 +6,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./shop.component.css']
 })
 export class ShopComponent {
+  showSecondRowBadges = false;
+  toggleSecondRowBadges() {
+    this.showSecondRowBadges = !this.showSecondRowBadges;
+  }
 
 }
