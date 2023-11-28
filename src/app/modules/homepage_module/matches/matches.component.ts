f2f13b30104adccc1504db13aacee08066fcc5df
boogle-noogle <abdelnaser.abdoa@gmail.com>
2023-11-28 15:46:44 +0100

    Cleaned up code

--- src/app/modules/homepage_module/matches/matches.component.ts
+++ src/app/modules/homepage_module/matches/matches.component.ts
@@ -10,7 +10,6 @@ import {
   startOfWeek,
   subDays,
 } from "date-fns";
-import { groupBy } from "lodash";
 import { Fixture } from "../../../core/models/fixtures.model";
 import { ApiService } from "../../../core/services/api.service";
 import { Router } from "@angular/router";
@@ -24,15 +23,13 @@ import { SupabaseFixture } from "../../../core/models/supabase-fixtures.model";
   providers: [DatePipe],
 })
 export class MatchesComponent implements OnInit {
-  fixtures: Fixture[] = [];
-  groupedFixtures: { [key: string]: SupabaseFixture[] } = {}; //grouped by date
-  groupedFixtureKeys: string[] = [];
   currentDate: Date;
   startDate: Date = new Date();
   endDate: Date = new Date();
   stringDate: string;
-  supabaseFixtures: SupabaseFixture[] = [];
-  groupSupabaseFixtures: { [key: string]: Fixture[] } = {}; //grouped by date
+  fixtures: SupabaseFixture[] = [];
+  groupedFixtures: { [key: string]: SupabaseFixture[] } = {}; //grouped by date
+  groupedFixtureKeys: string[] = [];
 
   constructor(
     private apiService: ApiService,
@@ -40,8 +37,7 @@ export class MatchesComponent implements OnInit {
     private router: Router,
     private fixtureTransferService: FixtureTransferService,
   ) {
-    this.currentDate = new Date(); //use this for today's date
-    // this.currentDate = new Date('2023-11-12'); //use this for specific date
+    this.currentDate = new Date();
     this.stringDate = this.currentDate.toISOString().split("T")[0];
   }
 
@@ -68,20 +64,6 @@ export class MatchesComponent implements OnInit {
     this.endDate = endOfWeek(date, { weekStartsOn: 5 });
   }
 
-  // fetchFixturesForWeek() {
-  //   let startDateString = this.getDateAsString(this.startDate);
-  //   let endDateString = this.getDateAsString(this.endDate);
-
-  //   // Fetch fixtures for the week range
-  //   this.apiService.fetchFixturesDateRange(144, startDateString, endDateString).subscribe(
-  //     (data: Fixture[]) => {
-  //       this.fixtures = data;
-  //       console.log(this.fixtures);
-  //       this.groupFixturesByDate();
-
-  //     }
-  //   );
-  // }
   async fetchFixturesForWeek() {
     let startDateString = this.getDateAsString(this.startDate);
     let endDateString = this.getDateAsString(this.endDate);
@@ -90,9 +72,9 @@ export class MatchesComponent implements OnInit {
         startDateString,
         endDateString,
       );
-      this.supabaseFixtures = fixtures;
+      this.fixtures = fixtures;
       this.groupFixturesByDate();
-      console.log(this.supabaseFixtures);
+      console.log(this.fixtures);
     } catch (error) {
       console.log(error);
     }
@@ -102,52 +84,33 @@ export class MatchesComponent implements OnInit {
     return Object.keys(this.groupedFixtures);
   }
 
-  // groupFixturesByDate() {
-  //   // Group the fixtures by date
-  //   this.groupedFixtures = groupBy(this.fixtures, (fixture) => {
-  //     return format(parseISO(fixture.fixture.date), 'yyyy-MM-dd');
-  //   });
-
-  //   // Sort the groups by date
-  //   this.groupedFixtureKeys = Object.keys(this.groupedFixtures).sort();
-
-  //   // Sort fixtures within each group by the full date and time
-  //   this.groupedFixtureKeys.forEach(date => {
-  //     this.groupedFixtures[date].sort((a, b) => compareAsc(parseISO(a.fixture.date), parseISO(b.fixture.date)));
-  //   });
-  // }
-
-
   groupFixturesByDate(): void {
-    // Clear the existing grouped fixtures
     this.groupedFixtures = {};
-  
-    // Iterate over the supabase fixtures
-    this.supabaseFixtures.forEach((fixture) => {
+
+    this.fixtures.forEach((fixture) => {
       // Ensure that time is a Date object
       const fixtureDate = new Date(fixture.time);
-  
+
       // Formats the date to YYYY-MM-DD format
       const dateKey = fixtureDate.toISOString().split("T")[0];
-  
+
       if (!this.groupedFixtures[dateKey]) {
         this.groupedFixtures[dateKey] = [];
       }
-  
+
       this.groupedFixtures[dateKey].push(fixture);
     });
-  
+
     // Sort the groups by date
     this.groupedFixtureKeys = Object.keys(this.groupedFixtures).sort();
-  
+
     // Sort fixtures within each group by the full date and time
-    this.groupedFixtureKeys.forEach(dateKey => {
-      this.groupedFixtures[dateKey].sort((a, b) => compareAsc(new Date(a.time), new Date(b.time)));
+    this.groupedFixtureKeys.forEach((dateKey) => {
+      this.groupedFixtures[dateKey].sort((a, b) =>
+        compareAsc(new Date(a.time), new Date(b.time))
+      );
     });
   }
-  
-  
-
 
   goToNextWeek() {
     this.setWeek(addDays(this.startDate, 7));
@@ -175,26 +138,5 @@ export class MatchesComponent implements OnInit {
   formatShortDateString(dateString: string): string {
     const date = new Date(dateString);
     return date.toLocaleDateString("en-US", { month: "short", day: "numeric" });
-  }
-  //--------------------------------------------- everything below this line is not used it is for one day for fixtures only so it's not used---------------------------------------------
-  parseFixtures(leagueID: number) {
-    let dateString = this.getDateAsString(this.currentDate);
-    console.log(dateString);
-    this.apiService.fetchFixtures(leagueID, dateString).subscribe(
-      (data: Fixture[]) => {
-        this.fixtures = data;
-        console.log(this.fixtures);
-      },
-    );
-  }
-
-  goToNextDay() {
-    this.currentDate.setDate(this.currentDate.getDate() + 1);
-    this.parseFixtures(144);
-  }
-
-  goToPreviousDay() {
-    this.currentDate.setDate(this.currentDate.getDate() - 1);
-    this.parseFixtures(144);
   }
 }
