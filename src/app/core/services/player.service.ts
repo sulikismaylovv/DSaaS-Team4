97af2ba6a457ac90ae8537d244c254aa6881767f
faresabumariam <faresabumariam02@gmail.com>
2023-11-29 21:58:27 +0100

    fetching fav club okay, fetching its players no data returned

---
+++ src/app/core/services/player.service.ts
@@ -0,0 +1,44 @@
+import { Injectable } from '@angular/core';
+import { SupabaseService } from './supabase.service';
+
+export interface Player {
+  id?: number;        // Assuming 'id' is an auto-incremented primary key
+  name: string;      // Player's name
+  club: number;      // Club's ID, which is a foreign key reference to a club table
+  age: number;       // Player's age
+  number: number;    // Player's jersey number
+  position: string;  // Player's position on the field
+  photo: string;     // URL to the player's photo
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PlayerService {
+
+  constructor(
+    private supabase: SupabaseService,
+  ) { }
+
+  async fetchPlayersByClubId(clubId: number): Promise<Player[]> {
+    try {
+      const { data, error } = await this.supabase.supabaseClient
+        .from('players')
+        .select('*')
+        .eq('club', clubId);
+
+      if (error) {
+        console.error('Error fetching players for club:', error);
+        throw error;
+      }
+      console.log("Data:", data);
+      return data as Player[];
+    } catch (error) {
+      console.error('Error fetching players by club ID:', error);
+      throw error;
+    }
+  }
+
+
+
+}
--- src/app/modules/shop/shop.component.ts
+++ src/app/modules/shop/shop.component.ts
@@ -1,14 +1,89 @@
 import { Component } from '@angular/core';
+import { OnInit } from '@angular/core';
+import {BetsService} from "../../core/services/bets.service";
+import {AuthService} from "../../core/services/auth.service";
+import {Player} from "../../core/services/player.service";
+import {PlayerService} from "../../core/services/player.service";
+import {PreferencesService} from "../../core/services/preference.service";
 
+export interface Club {
+  id?: number;
+  name: string;
+  code: string;
+  country: string;
+  founded: number;
+  national: boolean;
+  logo: string;
+  leagueID: number;
+}
 @Component({
   selector: 'app-shop',
   templateUrl: './shop.component.html',
   styleUrls: ['./shop.component.css']
 })
-export class ShopComponent {
+export class ShopComponent implements OnInit {
+  userCredits: number = 0;
+  currentUserID: string | undefined;
+  players: Player[] = []; // Define Player model according to your data structure
+  favoriteClub: Club | undefined;
+
   showMoreBadges = false;
-  toggleMoreBadges() {
-    this.showMoreBadges = !this.showMoreBadges;
+
+  constructor(
+    private betsService: BetsService,
+    private authService: AuthService,
+    private playerService: PlayerService,
+    private preferencesService: PreferencesService
+  ) {
   }
 
+  ngOnInit() {
+    this.initializeUser();
+  }
+
+  async initializeUser() {
+    this.currentUserID = this.authService.session?.user?.id;
+    console.log("Current user ID:", this.currentUserID);
+
+    if (this.currentUserID) {
+      await this.loadUserCredits();
+      await this.loadUserFavoriteClub();
+      await this.loadPlayersForFavoriteClub();
+    }
+  }
+
+  async loadUserFavoriteClub() {
+    if (this.currentUserID) {
+      const preferences = await this.preferencesService.getPreferences(this.currentUserID);
+      console.log("User preferences:", preferences);
+
+      const favoriteClubPreference = preferences.find(pref => pref.favorite_club);
+      if (favoriteClubPreference && favoriteClubPreference.club_id) {
+        this.favoriteClub = await this.preferencesService.getClubByClubId(parseInt(favoriteClubPreference.club_id));
+        console.log("Favorite club:", this.favoriteClub);
+      } else {
+        console.log("No favorite club preference found.");
+      }
+    }
+  }
+
+  async loadPlayersForFavoriteClub() {
+    if (this.favoriteClub && this.favoriteClub.id) {
+      console.log("Fetching players for club ID:", this.favoriteClub.id);
+      this.players = await this.playerService.fetchPlayersByClubId(this.favoriteClub.id);
+      console.log("Players for favorite club:", this.players);
+    } else {
+      console.log("No favorite club or club ID is undefined.");
+    }
+  }
+
+
+  async loadUserCredits() {
+    if (this.currentUserID) {
+      this.userCredits = await this.betsService.getUserCredits(this.currentUserID);
+      console.log("User credits:", this.userCredits);
+    } else {
+      console.log("Current user ID is undefined.");
+    }
+  }
 }
