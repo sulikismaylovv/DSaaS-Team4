5484e92e93ff59ff9010daa853cc0ac217fd0745
Suleyman Ismayilov <suleyman.ismayilov@student.kuleuven.be>
2023-12-05 20:18:18 +0100

    Main.ts fixed

--- src/main.ts
+++ src/main.ts
@@ -6,33 +6,7 @@ import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AuthService } from './app/core/services/auth.service';
 
-
-
-
-  platformBrowserDynamic().bootstrapModule(AppModule)
-    .catch(err => console.error(err));
-
-  if (environment.production) {
-    enableProdMode();
-  }
-// // Bootstrap the AppModule
-// platformBrowserDynamic().bootstrapModule(AppModule)
-//   .catch(err => console.error(err));
-//
-// //Fetch the session from AuthService
-// platformBrowserDynamic().bootstrapModule(AppModule)
-//   .then(moduleRef => {
-//     const authService = moduleRef.injector.get(AuthService);
-//     const session = authService.session;
-//
-//     // Use the session as needed (e.g., send it to Google Analytics)
-//     if (session) {
-//       // Send session to Google Analytics or perform other actions
-//       console.log('Session:', session.user.id);
-//       // Your Google Analytics code here
-//     }
-//   })
-//   .catch(err => console.error(err));
+// Extend the Window interface with the custom property
 interface CustomWindow extends Window {
   userId: string | null;
 }
@@ -57,10 +31,6 @@ platformBrowserDynamic().bootstrapModule(AppModule)
       window.userId = session?.user?.id || null;
 
       console.log('Session:', window.userId);
-
-      // Bootstrap the AppModule after setting the global variable
-      platformBrowserDynamic().bootstrapModule(AppModule)
-        .catch(err => console.error(err));
     });
   })
   .catch(err => console.error(err));
