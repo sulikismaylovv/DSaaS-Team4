286e2d914ce037d535b28fbb6df8717f70bffb6a
Suleyman Ismayilov <suleyman.ismayilov@student.kuleuven.be>
2023-12-08 00:12:06 +0100

    Some Tests Implemented + CI/CD updated

--- src/app/core/services/friends-league.service.spec.ts
+++ src/app/core/services/friends-league.service.spec.ts
@@ -1,16 +1,31 @@
 import { TestBed } from '@angular/core/testing';
+import { FriendsLeagueServiceService, FriendsLeagueInterface, UserInFriendsLeague } from './friends-league.service';
+import { SupabaseService } from 'src/app/core/services/supabase.service';
+import { AuthService } from './auth.service';
 
-import { FriendsLeagueServiceService } from './friends-league.service';
-
-describe('FriendsLeagueServiceService', () => {
+describe('FriendsLeagueService', () => {
   let service: FriendsLeagueServiceService;
+  let mockSupabaseService: jasmine.SpyObj<SupabaseService>;
+  let mockAuthService: jasmine.SpyObj<AuthService>;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    mockSupabaseService = jasmine.createSpyObj('SupabaseService', ['supabaseClient']);
+    mockAuthService = jasmine.createSpyObj('AuthService', ['session']);
+
+    TestBed.configureTestingModule({
+      // Provide both the service being tested and its dependencies
+      providers: [
+        FriendsLeagueServiceService,
+        { provide: SupabaseService, useValue: mockSupabaseService },
+        { provide: AuthService, useValue: mockAuthService }
+      ]
+    });
     service = TestBed.inject(FriendsLeagueServiceService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
+
+  // Additional tests...
 });
