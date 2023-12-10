3e6b29ccefa92306950ef7df1d1711d1a4aea369
chrysovalantis.psilos <chrysovalantis.psilos@student.kuleuven.be>
2023-12-10 23:31:28 +0100

    Help page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -19,34 +19,37 @@ import {ProfileComponent} from "./modules/profile_module/profile_page/profile.co
 import {NotificationComponent} from "./modules/notification-module/notification/notification.component";
 import {FirstSignGuard} from "./core/guards/first-sign.guard";
 import {ErrorComponent} from "./modules/error/error.component";
+import {HelppageComponent} from "./modules/helppage_module/helppage/helppage.component";
+import {FAQPageComponentComponent} from "./modules/helppage_module/faqpage-component/faqpage-component.component";
 
 
 const routes: Routes = [
-    { path: 'login', component: LoginComponent, pathMatch: 'full'},
-    { path: 'dashboard', component: DashboardComponent, pathMatch: 'full', canActivate: [ProfileGuard]},
-    { path: 'register', component: AuthComponent, pathMatch: 'full'},
-    { path: 'complete-profile', component: MultistepformComponent, pathMatch: 'full'},
-    { path: '', redirectTo: '/home', pathMatch: 'full',},
-    { path: 'home', component: HomeComponent, pathMatch: 'full'},
-    { path: 'verify-email', component: VerifyEmailComponent, pathMatch: 'full'},
-    {path: 'game/:id', component: GameComponent, pathMatch: 'full', canActivate: [ProfileGuard]},
-    { path: 'posts', component: PostsComponent, pathMatch: 'full'},
-    { path: 'games', component: MatchesComponent, pathMatch: 'full'},
-    {path: 'post/:id', component: SinglePostComponent, canActivate: [ProfileGuard]},
-    { path: 'shop', component: ShopComponent, pathMatch: 'full' },
-    {path: 'league', component: GloballeagueComponent, pathMatch: 'full', canActivate: [ProfileGuard]},
-    { path: 'settings', component: SettingsComponent, canActivate: [ProfileGuard], pathMatch: 'full'},
-    { path: 'profile', component: ProfileComponent, canActivate: [ProfileGuard, FirstSignGuard]},
-    {path: 'profile/:userId', component: ProfileComponent, canActivate: [ProfileGuard]},
-    { path: 'notifications', component: NotificationComponent, canActivate: [ProfileGuard]},
-    { path: '**', component: ErrorComponent, pathMatch: 'full'},
+  { path: 'login', component: LoginComponent, pathMatch: 'full'},
+  { path: 'dashboard', component: DashboardComponent, pathMatch: 'full', canActivate: [ProfileGuard]},
+  { path: 'register', component: AuthComponent, pathMatch: 'full'},
+  { path: 'complete-profile', component: MultistepformComponent, pathMatch: 'full'},
+  { path: '', redirectTo: '/home', pathMatch: 'full',},
+  { path: 'home', component: HomeComponent, pathMatch: 'full'},
+  { path: 'verify-email', component: VerifyEmailComponent, pathMatch: 'full'},
+  {path: 'game/:id', component: GameComponent, pathMatch: 'full', canActivate: [ProfileGuard]},
+  { path: 'posts', component: PostsComponent, pathMatch: 'full'},
+  { path: 'games', component: MatchesComponent, pathMatch: 'full'},
+  {path: 'post/:id', component: SinglePostComponent, canActivate: [ProfileGuard]},
+  { path: 'shop', component: ShopComponent, pathMatch: 'full' },
+  {path: 'league', component: GloballeagueComponent, pathMatch: 'full', canActivate: [ProfileGuard]},
+  { path: 'settings', component: SettingsComponent, canActivate: [ProfileGuard], pathMatch: 'full'},
+  { path: 'profile', component: ProfileComponent, canActivate: [ProfileGuard, FirstSignGuard]},
+  {path: 'profile/:userId', component: ProfileComponent, canActivate: [ProfileGuard]},
+  { path: 'notifications', component: NotificationComponent, canActivate: [ProfileGuard]},
+  {path:'help', component: FAQPageComponentComponent, pathMatch: 'full'},
+  { path: '**', component: ErrorComponent, pathMatch: 'full'},
 
 ];
 
 @NgModule({
-    imports: [RouterModule.forRoot(routes)],
-    exports: [RouterModule],
-    providers: [ProfileGuard]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  providers: [ProfileGuard]
 })
 export class AppRoutingModule {
 }
