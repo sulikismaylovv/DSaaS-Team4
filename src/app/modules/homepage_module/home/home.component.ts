7be9d158de2e086524d5ead8ff3b47c90fdcd881
faresabumariam <faresabumariam02@gmail.com>
2023-12-12 00:09:36 +0100

    remove unused stuff after splitting aside

--- src/app/modules/homepage_module/home/home.component.ts
+++ src/app/modules/homepage_module/home/home.component.ts
@@ -1,16 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from "@angular/router";
-import {AuthService} from "../../../core/services/auth.service";
-import {ThemeService} from "../../../core/services/theme.service";
 import {NavbarService} from "../../../core/services/navbar.service";
-import {UserServiceService} from "../../../core/services/user-service.service";
-import {ImageDownloadService} from "../../../core/services/imageDownload.service";
 import {SupabaseFixture, SupabaseFixtureModel} from "../../../core/models/supabase-fixtures.model";
 import {Club} from "../../../core/models/club.model";
-import {FixtureTransferService} from "../../../core/services/fixture-transfer.service";
-import {ApiService} from "../../../core/services/api.service";
-import {PreferencesService} from "../../../core/services/preference.service";
-import {Preference} from "../../../core/services/preference.service";
 import {DailyAwardService} from "../../../core/services/daily-award.service";
 
 @Component({
@@ -19,28 +11,14 @@ import {DailyAwardService} from "../../../core/services/daily-award.service";
     styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
-  fixture: SupabaseFixture = new SupabaseFixtureModel();
   league: Club[] = [];
-
-
-  session: any; // Adjust the type based on your session object
-  hideForm = false;
   showPosts = false;
   showMatches = true;
   activeContent = 'matches';
-  userSearchResults: any[] = [];
 
   constructor(
-    public themeService: ThemeService,
     public navbarService: NavbarService,
     private router: Router,
-    protected readonly authService: AuthService,
-    private preferencesService: PreferencesService,
-    protected readonly userService: UserServiceService,
-    protected readonly imageDownloadService: ImageDownloadService,
-    private fixtureTransferService: FixtureTransferService,
-    private route: ActivatedRoute,
-    private apiService: ApiService,
     private dailyAwardService: DailyAwardService
 
   ) {
@@ -49,32 +27,6 @@ export class HomeComponent implements OnInit {
     this.dailyAwardService.openPopup();
     this.navbarService.setShowNavbar(true);
 
-  }
-
-
-
-
-
-
-  // async getNextFixture(clubID: number) {
-  //   try {
-  //     this.nextFixture =(await this.apiService.testFunction(clubID))[0];
-  //     console.log("This. nect ficture", this.nextFixture);
-  //   } catch (error) {
-  //     console.error('Error fetching next fixture:', error);
-  //   }
-  //   this.nextClub = this.getNextOpponentClub();
-  //   this.myClub = this.getMyClub();
-  // }
-
-  // async getStanding() {
-  //   const data = await this.apiService.fetchStandings();
-  //   this.league = data;
-  //   console.log(this.league);
-  // }
-
-  async navigateToLogin() {
-    await this.router.navigateByUrl('/login'); // Adjust the path as necessary for your app's route configuration
   }
 
   showPostsContent() {
@@ -88,18 +40,6 @@ export class HomeComponent implements OnInit {
     this.showPosts = false;
     this.showMatches = true;
   }
-
-  // async onUserSearch(event: any): Promise<void> {
-  //   const searchTerm = event.target.value;
-  //   if (searchTerm.length > 2) { // Trigger search when at least 3 characters are typed
-  //     this.userSearchResults = await this.userService.searchUsersByFirstThreeLetters(searchTerm);
-  //     for (const user of this.userSearchResults) {
-  //           user.avatarSafeUrl = await this.imageDownloadService.loadAvatarImage(user.id);
-  //     }
-  //   } else {
-  //     this.userSearchResults = [];
-  //   }
-  // }
 
 
 
