e700e98e8480ad3a612e284a00d3a11f5e37cdfb
Suleyman Ismayilov <100700575+sulikismaylovv@users.noreply.github.com>
2023-12-11 01:28:24 +0100

    Update environment.development.ts

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,8 +1,8 @@
 export const environment = {
     production: false,
-    supabaseUrl: 'https://kvjhoifmabxitkelfeod.supabase.co',
+    supabaseUrl: '',
     appTitle: 'Remontada (preview)',
-    supabaseKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imt2amhvaWZtYWJ4aXRrZWxmZW9kIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTcxMzQ3NDMsImV4cCI6MjAxMjcxMDc0M30.vttAJafQbMLkJFQU0r49cRjKa9fHcmtIQYkcNyAo5A4',
+    supabaseKey: '',
     baseUrl: 'http://localhost:4200/',
 
 }
