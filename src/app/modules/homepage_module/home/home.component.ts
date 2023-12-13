03358aeacebfc0a24c0bebd2be4a00d89c086e13
faresabumariam <faresabumariam02@gmail.com>
2023-12-13 13:05:45 +0100

    added user search in home

--- src/app/modules/homepage_module/home/home.component.ts
+++ src/app/modules/homepage_module/home/home.component.ts
@@ -3,6 +3,9 @@ import {Router} from "@angular/router";
 import {NavbarService} from "../../../core/services/navbar.service";
 import {Club} from "../../../core/models/club.model";
 import {DailyAwardService} from "../../../core/services/daily-award.service";
+import {AuthService} from "../../../core/services/auth.service";
+import {UserServiceService} from "../../../core/services/user-service.service";
+import {ImageDownloadService} from "../../../core/services/imageDownload.service";
 
 @Component({
     selector: 'app-home',
@@ -11,6 +14,7 @@ import {DailyAwardService} from "../../../core/services/daily-award.service";
 })
 export class HomeComponent implements OnInit {
   league: Club[] = [];
+  userSearchResults: any[] = [];
   showPosts = false;
   showMatches = true;
   activeContent = 'matches';
@@ -18,14 +22,28 @@ export class HomeComponent implements OnInit {
   constructor(
     public navbarService: NavbarService,
     private router: Router,
-    private dailyAwardService: DailyAwardService
-
-  ) {
+    private dailyAwardService: DailyAwardService,
+    protected readonly authService: AuthService,
+    protected readonly userService: UserServiceService,
+    protected readonly imageDownloadService: ImageDownloadService,
+) {
   }
   ngOnInit() {
     this.dailyAwardService.openPopup();
     this.navbarService.setShowNavbar(true);
 
+  }
+
+  async onUserSearch(event: any): Promise<void> {
+    const searchTerm = event.target.value;
+    if (searchTerm.length > 2) { // Trigger search when at least 3 characters are typed
+      this.userSearchResults = await this.userService.searchUsersByFirstThreeLetters(searchTerm);
+      for (const user of this.userSearchResults) {
+        user.avatarSafeUrl = await this.imageDownloadService.loadAvatarImage(user.id);
+      }
+    } else {
+      this.userSearchResults = [];
+    }
   }
 
   showPostsContent() {
